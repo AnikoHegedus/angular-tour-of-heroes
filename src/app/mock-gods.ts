11f12a80281aa4444305f7795447cde9d00b15f0
AnikoHegedus <millkamill@hotmail.com>
2018-03-29 16:05:46 +0200

    details of heroes and gods
    plus css

---
+++ src/app/mock-gods.ts
@@ -0,0 +1,9 @@
+import { God } from './god';
+
+export const GODS: God[] = [
+  { id: 11, name: 'Zeus' },
+  { id: 12, name: 'Apollon' },
+  { id: 13, name: 'Pallas Athene' },
+  { id: 14, name: 'Persephone' },
+  { id: 15, name: 'Hades' }
+];
