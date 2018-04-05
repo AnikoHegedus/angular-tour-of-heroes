79321708967be533e5ebb5cb265620519c198825
AnikoHegedus <millkamill@hotmail.com>
2018-04-05 16:04:44 +0200

    styling

--- src/app/mock-gods.ts
+++ src/app/mock-gods.ts
@@ -2,8 +2,13 @@ import { God } from './god';
 
 export const GODS: God[] = [
   { id: 11, name: 'Zeus' },
-  { id: 12, name: 'Apollon' },
-  { id: 13, name: 'Pallas Athene' },
+  { id: 12, name: 'Apollo' },
+  { id: 13, name: 'Athena' },
   { id: 14, name: 'Persephone' },
-  { id: 15, name: 'Hades' }
+  { id: 15, name: 'Ares' },
+  { id: 16, name: 'Amor' },
+  { id: 17, name: 'Hera' },
+  { id: 18, name: 'Hades' },
+  { id: 19, name: 'Artemis' },
+  { id: 20, name: 'Demeter' }
 ];
